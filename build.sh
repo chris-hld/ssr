f0f27acd1dcd3dbc9c4dbba9ab86ffeb69ef220c
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2018-01-24 15:45:53 +0100

    Add Cross Compilation Toolchain

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -r _build
 mkdir -p _build && cd _build
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/chris/git/ssr-chris/Toolchain-mingw64.cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j8
 cd ..
