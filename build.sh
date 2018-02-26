55f56c0b2ade8126fb6faa41c2a78f56ae11f880
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2018-02-26 11:56:18 +0100

    Working MXE toolchain

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 rm -r _build
 mkdir -p _build && cd _build
-cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+export PATH=/home/chris/git/mxe/usr/bin:$PATH
+#cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+x86_64-w64-mingw32.static.posix-cmake ... -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
 make -j8
 cd ..
