5f1f61e664bd323921cf5961955fd90bdbffc4ea
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-01-29 19:24:54 +0100

    CI: Remove rm

--- ci/build.sh
+++ ci/build.sh
@@ -7,7 +7,6 @@ then
     then
         PATH=$PATH:/usr/lib/mxe/usr/bin
         cd ..
-        rm -r _build
         mkdir -p _build && cd _build
         x86_64-w64-mingw32.static-cmake ... -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
         make
