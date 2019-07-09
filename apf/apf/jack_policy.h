a310ae74eefc42c3d1a96580c2b5c64f02f7fb55
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-07-09 11:30:28 +0300

    Fix Jack handling priority under windows

--- apf/apf/jack_policy.h
+++ apf/apf/jack_policy.h
@@ -121,10 +121,15 @@ struct thread_traits<jack_policy, pthread_t>
     {
       struct sched_param param;
       param.sched_priority = obj.get_real_time_priority();
+      #ifndef _WIN32
       if (pthread_setschedparam(thread_id, SCHED_FIFO, &param))
       {
         throw std::runtime_error("Can't set scheduling priority for thread!");
       }
+      #else
+      // TODO: Windows scheduling
+        printf("Realtime scheduling not available under Windows.\n");
+      #endif
     }
     else
     {
