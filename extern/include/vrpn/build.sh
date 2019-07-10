c549c152ba6535045c43c25b8ee5378c7fc00ca7
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-07-10 16:32:09 +0300

    Add vrpn source and libs

---
+++ extern/include/vrpn/build.sh
@@ -0,0 +1,17 @@
+# Configure MXE path:
+MXE_PATH=/home/chris/git/mxe
+
+
+PATH=$MXE_PATH/usr/bin:$PATH
+
+rm -r _build
+mkdir -p _build && cd _build
+
+#cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64.cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+x86_64-w64-mingw32.static-cmake ... -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+make -j1
+
+#x86_64-w64-mingw32.static.posix-cmake ... -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+#make
+
+cd ..
